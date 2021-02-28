32bc4dd7dc1f7e827b903263812a60df239d26e8
Fidellis-Taz <fidellis.dango@gmail.com>
2021-02-28 20:19:11 +0300

    images fetched and displayed for all titles

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,15 @@ import request from "./api/request";
 function App() {
   return (
     <div className="app">
-      <h1>React app</h1>
+      <h1>Movies</h1>
       <Row title="NETLIX ORIGINALS" fetchURL={request.fetchNetlixOriginals} />
       <Row title="Trending Now" fetchURL={request.fetchTopRated} />
+      <Row title="Top Rated" fetchURL={request.fetchTopRated} />
+      <Row title="Action Movies" fetchURL={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchURL={request.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={request.fetchDocumentaries} />
     </div>
   );
 }
--- client/src/components/Row.js
+++ client/src/components/Row.js
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "../api/axios";
+import "./Row.css";
+const base_url = "https://image.tmdb.org/t/p/original/";
+
 const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const request = await axios.get(fetchURL);
-      console.log(request.data);
-      setMovies(request.data.resuslts);
+      console.log(request.data.results);
+
+      setMovies(request.data.results);
     };
     fetchData();
 
@@ -15,8 +19,21 @@ const Row = ({ title, fetchURL }) => {
   }, [fetchURL]);
 
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
+
+      <div className="row__posters">
+        {movies &&
+          movies.map((movie) => (
+            <img
+              /* each img must have an identity so that when one image changes react can identify the change and simply update faster */
+              key={movie.id}
+              className="row__poster"
+              src={`${base_url}${movie.poster_path}`}
+              alt={movie.name}
+            />
+          ))}
+      </div>
     </div>
   );
 };
