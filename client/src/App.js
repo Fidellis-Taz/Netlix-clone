2b1f780abbec69dd3a0dc158cb5bb1b20dc6c1bd
Fidellis-Taz <fidellis.dango@gmail.com>
2021-03-03 16:02:33 +0300

    done

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,11 @@ import Row from "./components/Row";
 import request from "./api/request";
 import Banner from "./components/Banner";
 import "./App.css";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="app">
+      <Navbar />
       <Banner />
 
       <Row
@@ -13,7 +15,7 @@ function App() {
         fetchURL={request.fetchNetlixOriginals}
         isLargeRow
       />
-      <Row title="Trending Now" fetchURL={request.fetchTopRated} />
+      <Row title="Trending Now" fetchURL={request.fetchTrending} />
       <Row title="Top Rated" fetchURL={request.fetchTopRated} />
       <Row title="Action Movies" fetchURL={request.fetchActionMovies} />
       <Row title="Comedy Movies" fetchURL={request.fetchComedyMovies} />
--- client/src/components/Row.js
+++ client/src/components/Row.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 import axios from "../api/axios";
 import "./Row.css";
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 const Row = ({ title, fetchURL, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,7 +20,27 @@ const Row = ({ title, fetchURL, isLargeRow }) => {
 
     /* whenever we are using a variable from outside the blokc we should pass it as aa dependency as it changes  */
   }, [fetchURL]);
-
+  /* for youtube  */
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  const handleClick = (movie) => {
+    /* if you have the video open then close is */
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name || " ")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((err) => console.log(err));
+    }
+  };
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -28,7 +51,9 @@ const Row = ({ title, fetchURL, isLargeRow }) => {
             <img
               /* each img must have an identity so that when one image changes react can identify the change and simply update faster */
               key={movie.id}
+              onClick={() => handleClick(movie)}
               /* every item will get a row__poster clss name but it its isLargeRow we will add another class name besides it  */
+
               className={`row__poster ${isLargeRow && "row__posterLarge"}`}
               src={`${base_url}${
                 isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -37,6 +62,8 @@ const Row = ({ title, fetchURL, isLargeRow }) => {
             />
           ))}
       </div>
+
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
